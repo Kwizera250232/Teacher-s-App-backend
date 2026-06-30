dfa0b030b19f15c94d78a429281f70e05af44d82
Kwizera250232 <kwizerajeandedieu250@gmail.com>
2026-06-30 19:25:25 +0200

    fix: alumni-compositions.js pool import (same bug as alumni.js)

--- routes/alumni-compositions.js
+++ routes/alumni-compositions.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { pool } = require('../db');
+const pool = require('../db');
 const { authenticateToken, requireRole } = require('../middleware/auth');
 
 function audit(event, details) {
